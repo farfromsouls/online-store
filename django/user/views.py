e825dba4566e68471e121891ee9eb55d9c88c736
farfromsouls <farfromsouls@gmail.com>
2026-03-22 22:00:10 +0700

    Saving bucket when switch from anon session to account

--- django/user/views.py
+++ django/user/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import login, logout, authenticate
 from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_protect
+from django.contrib.sessions.models import Session
 from .models import CustomUser
 
 # Create your views here.
@@ -10,6 +11,7 @@ def Account(request):
 @csrf_protect
 def Login(request):
     if request.method == "POST":
+        
         username=request.POST.get("username")
         password=request.POST.get("password")
         
@@ -17,12 +19,24 @@ def Login(request):
         
         if user == None:
             return redirect('account')
+        
+        session_bucket = request.session.get('bucket', {})
+        
+        result_bucket = {}
+        for key in set(session_bucket) | set(user.Bucket):  
+            result_bucket[key] = session_bucket.get(key, 0) + user.Bucket.get(key, 0)
+        user.Bucket = result_bucket
+        user.save()
+        if 'bucket' in request.session:
+            del request.session['bucket']
+        
         login(request, user)
-        return redirect('account')
+        
+        
+    return redirect('account')
 
 @csrf_protect
 def Register(request):
-    print(request.POST)
     username=request.POST.get("username")
     email=request.POST.get("email")
     password1=request.POST.get("password1")
